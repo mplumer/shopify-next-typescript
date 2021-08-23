00b3e5bb705064ef9d1e5db223d25c8d2fb00d71
Max Plumer <maxplumer12@gmail.com>
2021-08-23 14:05:54 -0400

    begin shopify framework

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 
-export default function Home() {
+import type { InferGetStaticPropsType } from "next"
+import getAllProducts from "../framework/shopify/product/get-all-products"
 
-  const message: string = "Hello, " 
+export async function getStaticProps() {
+  const products = await getAllProducts()
 
-  let person: Person = {
-    name: "Max"
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
   }
+}
+
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
     <div>
-      {message}
-      {person.name}
+      { products }
     </div>
   )
 }
