63f9db3b6c488fa9976a332ea5f2789eb882bf4e
Max Plumer <maxplumer12@gmail.com>
2021-08-27 21:46:25 -0400

    connect storefront api

--- framework/common/types/product.ts
+++ framework/common/types/product.ts
@@ -4,6 +4,33 @@ export interface ProductImage {
   alt?: string
 }
 
+export interface ProductPrice {
+  value: number
+  currencyCode: "USD" | "EUR" | string
+}
+
+export interface ProductOptionValues {
+  label: string
+  hexColor?: string
+}
+
+export interface ProductOption {
+  id: string
+  displayName: string
+  values: ProductOptionValues[]
+}
+
+export interface ProductVariant {
+  id: string
+  name: string
+  sku: string
+  image?: ProductImage
+  requiresShipping: boolean
+  price: number
+  listPrice: number
+  options: ProductOption[]
+}
+
 export interface Product {
   id: string
   name: string
@@ -11,4 +38,7 @@ export interface Product {
   slug: string
   path: string
   images: ProductImage[]
+  price: ProductPrice,
+  options: ProductOption[],
+  variants: ProductVariant[]
 }
--- framework/shopify/utils/fetch-api.ts
+++ framework/shopify/utils/fetch-api.ts
@@ -1,24 +1,24 @@
-
-type FetcherParams = {
-  query: string
-}
-
-type FetcherResult<T> = { data: T}
+import {
+  ApiFetcherOptions,
+  ApiFetcherResults
+} from "@common/types/api"
+import { API_URL, STOREFRONT_TOKEN } from "@framework/const"
 
 const fetchApi = async <T>({
-  query }: FetcherParams
-): Promise<FetcherResult<T>> => {
-  const url = "http://localhost:4000/graphql"
-  const res = await fetch(url, {
+  query,
+  variables }: ApiFetcherOptions
+): Promise<ApiFetcherResults<T>> => {
+  const res = await fetch(API_URL!, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "X-Shopify-Storefront-Access-Token": STOREFRONT_TOKEN!
     },
     body: JSON.stringify({
-      query
+      query,
+      variables
     })
   })
-
   const { data, errors} = await res.json()
   // ?? is checking if left hand expression is null or undefined -> if it is go with right expression
   // || is checking if left hand expression is null, undefined, "", 0, false
